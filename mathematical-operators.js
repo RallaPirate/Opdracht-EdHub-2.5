ab35144ce5a7e61ed3052ff4dd45f948a42f3f06
RallaPirate <tantegwen@gmail.com>
2024-10-20 18:48:13 +0200

    Complete uitwerkingen

--- mathematical-operators.js
+++ mathematical-operators.js
@@ -4,20 +4,42 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // 1. Declareer een variabele en sla daar de uitkomst van 4 plus 5 in op.                           // geeft 9 wanneer gelogd
 // ==========================================
 
+console.log("Opdracht 1:");
+console.log("------------");
+const result = 4+5;
+console.log(result);
+console.log(" ");
 
 // ==========================================
 // 2. Declareer een variabele en sla daar de uitkomst van 7 maal 52 in op.                          // geeft 364 wanneer gelogd
 // ==========================================
 
+console.log("Opdracht 2:");
+console.log("------------");
+const result2 = 7*52;
+console.log(result2);
+console.log(" ");
 
 // ==========================================
 // 3. Declareer een variabele en sla daar de uitkomst van 4 + 3 in op, vermenigvuldigd met 7.       // geeft 49 wanneer gelogd
 // ==========================================
 
+console.log("Opdracht 3:");
+console.log("------------");
+const result3 = (4+3)*7;
+console.log(result3);
+console.log(" ");
+
 
 // ==========================================
 // 4. Declareer een variabele en sla daar de uitkomst van 36 gedeeld door 6, maal 2 in op.          // geeft 12 wanneer gelogd
 // ==========================================
+
+console.log("Opdracht 4:");
+console.log("------------");
+const result4 = 36/6*2;
+console.log(result4);
+console.log(" ");
 
 
 // ==========================================
@@ -25,6 +47,15 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // Declareer nog een variabele en sla daarin de samenvoeging van bovenstaande variabelen in op.     // geeft 'zoetsappig' wanneer gelogd
 // ==========================================
 
+console.log("Opdracht 5:");
+console.log("------------");
+const flavor1 = "zoet";
+console.log(flavor1);
+const flavor2= "sappig";
+console.log(flavor2);
+const flavor = smaak1+smaak2;
+console.log(flavor);
+console.log(" ");
 
 // ==========================================
 // 6. Declareer een variabele voor jouw voornaam en een variabele voor jouw achternaam en zet de waardes erin.
@@ -32,9 +63,29 @@ console.log('Zorg dat al jouw uitkomsten één voor één in de terminal worden 
 // Zorg dat er een spatie tussen zit zonder iets aan de originele variabelen te veranderen.        // geeft bijv. 'Henk Pieters' wanneer gelogd
 // ==========================================
 
+console.log("Opdracht 6:");
+console.log("------------");
+const firstName = "Gwendolyn";
+console.log(firstName);
+const lastName = "Janssen";
+console.log(lastName);
+const fullName = firstName + " " + lastName;
+console.log(fullName);
+console.log(" ");
+
 
 // ==========================================
 // 7. Declareer een variabele voor jouw woonplaats en een variabele voor provincie en zet de waardes erin.
 // Declareer vervolgens een variabele voor jouw locatie en plak jouw de woonplaats en provincie aan elkaar.
 // Zorg dat er een komma en spatie tussen zit zonder iets aan de originele variabelen te veranderen. // geeft bijv. Maastricht, Limburg wanneer gelogd
 // ==========================================
+
+console.log("Opdracht 7:");
+console.log("------------");
+const place = "Vinkenbuurt";
+console.log(place);
+const province = "Overijssel";
+console.log(province);
+const location = place + ", " + province;
+console.log(location);
+console.log(" ");
--- variables.js
+++ variables.js
@@ -7,38 +7,82 @@ const start = "Let's go!";
 // ==========================================
 // 1. Declareer een variabele om jouw naam in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
+console.log("Opdracht 1:");
+console.log("------------");
+const name = "Gwendolyn Janssen";
+console.log(name);
+console.log(" ");
 
 
 // ==========================================
 // 2. Declareer een variabele om de kleur van jouw ogen in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
+console.log("Opdracht 2:");
+console.log("------------");
+const eyeColor = "brown";
+console.log(eyeColor);
+console.log(" ");
 
 
 // ==========================================
 // 3. Declareer een variabele om jouw leeftijd in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+console.log("Opdracht 3:");
+console.log("------------");
+let age = 37;
+console.log(age);
+console.log(" ");
+
 
 // ==========================================
 // 4. Declareer een variabele om jouw beroep in op te slaan en zet de waarde erin. De variabele moet later overschreven kunnen worden.
 // ==========================================
+
+console.log("Opdracht 4:");
+console.log("------------");
+let profession = "Student";
+console.log(profession);
+console.log(" ");
 
 
 // ==========================================
 // 5. Declareer een variabele om jouw schoenmaat in op te slaan en zet de waarde erin. De variabele hoeft niet meer overschreven hoeft te worden.
 // ==========================================
 
+console.log("Opdracht 5:");
+console.log("------------");
+const shoeSize = 36.5;
+console.log(shoeSize);
+console.log(" ");
+
 
 // ==========================================
 // 6. Declareer een variabele om in op te slaan of je een partner hebt of niet. De variabele moet later overschreven kunnen worden.
 // ==========================================
+
+console.log("Opdracht 6:");
+console.log("------------");
+let single = false;
+console.log(single);
+console.log(" ");
 
 
 // ==========================================
 // 7. Declareer een variabele om het aantal kinderen in op te slaan. De variabele moet later overschreven kunnen worden.
 // ==========================================
 
+console.log("Opdracht 7:");
+console.log("------------");
+let numberOfChildren = 0;
+console.log(numberOfChildren);
+console.log(" ");
+
 
 // ==========================================
 // 8. Gebruik jouw variabelen nu om hun waardes te loggen in de terminal. Klopt het allemaal?
 // ==========================================
+
+console.log("Opdracht 8:");
+console.log("------------");
+console.log("Naam: " + name + ", kleur ogen: " + eyeColor + ", leeftijd: " + age + ", beroep: " + profession + ", maat schoenen: " + shoeSize + ", alleenstaand: " + single + ", aantal kinderen: " + numberOfChildren);
